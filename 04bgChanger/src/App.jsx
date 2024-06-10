b3ee0a0622cb2e70a169128a45c27f2ffb746a8a
Anmol Maske <128815229+anmolmaske05@users.noreply.github.com>
2024-06-10 21:51:41 +0530

    spaces remove

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -13,19 +13,12 @@ function App() {
           <button onClick={() => { setColor("blue") }} className="outline-none px-4 rounded-full py-1" style={{ backgroundColor: "blue" }}>Blue</button>
           <button onClick={() => { setColor("pink") }} className="outline-none px-4 rounded-full py-1" style={{ backgroundColor: "pink" }}>Pink</button>
           <button onClick={() => { setColor("orange") }} className="outline-none px-4 rounded-full py-1" style={{ backgroundColor: "orange" }}>Orange</button>
-
           <button onClick={() => { setColor("purple") }} className="outline-none px-4 rounded-full py-1" style={{ backgroundColor: "purple" }}>Purple</button>
-
           <button onClick={() => { setColor("olive") }} className="outline-none px-4 rounded-full py-1" style={{ backgroundColor: "olive" }}>Olive</button>
-
-          <button onClick={() => { setColor("white") }} className="outline-4 px-4 rounded-full py-1" style={{ backgroundColor: "white" }}>White</button>
-
+          <button onClick={() => { setColor("white") }} className="outline-2 px-4 rounded-full py-1" style={{ backgroundColor: "white" }}>White</button>
           <button onClick={() => { setColor("gray") }} className="outline-none px-4 rounded-full py-1" style={{ backgroundColor: "gray" }}>Gray</button>
-
           <button onClick={() => { setColor("lightblue") }} className="outline-none px-4 rounded-full py-1" style={{ backgroundColor: "lightblue" }}>Light Blue</button>
-
           <button onClick={() => { setColor("yellow") }} className="outline-none px-4 rounded-full py-1" style={{ backgroundColor: "yellow" }}>Yellow</button>
-
           <button onClick={() => { setColor("lightpink") }} className="outline-none px-4 rounded-full py-1" style={{ backgroundColor: "lightpink" }}>Light Pink</button>
         </div>
       </div>
